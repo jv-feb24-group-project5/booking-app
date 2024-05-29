a04aa7b166dc896c236fdced389913c2e3847871
TonyH277 <robhobs50@gmail.com>
2024-05-29 20:20:25 +0300

    init commit

---
+++ src/test/java/com/ua/accommodation/BookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ua.accommodation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
