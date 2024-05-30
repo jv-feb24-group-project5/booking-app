996f18308731e4b08e6f581ada1708245f0c5eb9
Pavlo Betsa <bigworld2303@gmail.com>
2024-05-30 11:35:53 +0300

    Defined entity

--- src/main/java/com/ua/accommodation/model/Payment.java
+++ src/main/java/com/ua/accommodation/model/Payment.java
@@ -1,4 +1,18 @@
 package com.ua.accommodation.model;
 
+import java.math.BigDecimal;
+import java.net.URL;
+
 public class Payment {
+    private Long id;
+    private Status status;
+    private Long bookingId;
+    private URL SessionURL;
+    private String SessionId;
+    private BigDecimal AmountToPay;
+
+    public enum Status {
+        PENDING,
+        PAID
+    }
 }
