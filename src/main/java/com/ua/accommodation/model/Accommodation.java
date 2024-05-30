02956b8cb3dd0df77af54b0e2631b7b3eeb65aba
Pavlo_Betsa <151930464+Nikname2303@users.noreply.github.com>
2024-05-30 15:59:51 +0300

    added accomodation entitie with liquibase scripts

--- src/main/java/com/ua/accommodation/model/Accommodation.java
+++ src/main/java/com/ua/accommodation/model/Accommodation.java
@@ -1,4 +1,64 @@
 package com.ua.accommodation.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Set;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.hibernate.annotations.SoftDelete;
+
+@Entity
+@Table(name = "accommodations")
+@SoftDelete(columnName = "is_deleted")
+@Data
 public class Accommodation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Type type;
+
+    @OneToOne
+    @JoinColumn(name = "address_id", referencedColumnName = "id", nullable = false)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Address location;
+
+    @Column(nullable = false)
+    private String size;
+
+    @ManyToMany
+    @JoinTable(
+            name = "accommodations_amenities",
+            joinColumns = @JoinColumn(name = "accommodation_id"),
+            inverseJoinColumns = @JoinColumn(name = "amenity_id")
+    )
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Set<Amenity> amenities;
+
+    @Column(name = "daily_rate", nullable = false)
+    private BigDecimal dailyRate;
+
+    @Column(nullable = false)
+    private Integer availability;
+
+    public enum Type {
+        HOUSE, APARTMENT, CONDO, VACATION_HOME
+    }
 }
---
+++ src/main/java/com/ua/accommodation/model/Address.java
@@ -0,0 +1,33 @@
+package com.ua.accommodation.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "addresses")
+@Data
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String street;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(nullable = false, name = "zip_code")
+    private String zipCode;
+
+    @Column(nullable = false)
+    private String country;
+}
