b285c70622fab6fbf29561700c2a29bdb3413869
Pavlo Betsa <bigworld2303@gmail.com>
2024-05-30 15:20:25 +0300

    Defined entity, added dependencies for h2, postgres

--- src/main/java/com/ua/accommodation/model/Payment.java
+++ src/main/java/com/ua/accommodation/model/Payment.java
@@ -1,15 +1,43 @@
 package com.ua.accommodation.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.net.URL;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
+@Entity
+@Getter
+@Setter
+@Table(name = "payments")
+@SQLDelete(sql = "UPDATE payments SET is_deleted = true WHERE id=?")
+@SQLRestriction("is_deleted=false")
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Status status;
+    @Column(nullable = false)
     private Long bookingId;
-    private URL SessionURL;
-    private String SessionId;
-    private BigDecimal AmountToPay;
+    @Column(nullable = false)
+    private URL sessionUrl;
+    @Column(nullable = false)
+    private String sessionId;
+    @Column(nullable = false)
+    private BigDecimal amountToPay;
+    @Column(nullable = false)
+    private boolean isDeleted;
 
     public enum Status {
         PENDING,
