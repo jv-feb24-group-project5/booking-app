76204a81a707e09836430ab82b67c222915fec0e
Vladyslav Radko <124697095+VolandevlodD@users.noreply.github.com>
2024-05-31 11:32:39 +0300

    Task/Accomodation entity (#7)

---
+++ src/main/java/com/ua/accommodation/model/Amenity.java
@@ -0,0 +1,23 @@
+package com.ua.accommodation.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "amenities")
+@Getter
+@Setter
+public class Amenity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
