918f15e2a99485103dd4ecbda2288907cef5a0af
VolandevlodD <vladradko2000@gmail.com>
2024-06-01 20:11:59 +0300

    Removed redundant @DataAnnotation

--- src/main/java/com/ua/accommodation/model/Address.java
+++ src/main/java/com/ua/accommodation/model/Address.java
@@ -6,11 +6,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "addresses")
-@Data
+@Getter
+@Setter
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
