a72b499eaf6d7481b1122f959521d076edb64d3d
VolandevlodD <vladradko2000@gmail.com>
2024-06-03 21:35:06 +0300

    Scheduled event

--- src/main/java/com/ua/accommodation/BookingAppApplication.java
+++ src/main/java/com/ua/accommodation/BookingAppApplication.java
@@ -2,12 +2,13 @@ package com.ua.accommodation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BookingAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookingAppApplication.class, args);
     }
-
 }
