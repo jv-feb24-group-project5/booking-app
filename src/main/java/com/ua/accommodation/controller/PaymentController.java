6e7aa93fff2b69ea491a5893e35d0300d25e5760
MolchanovAlexander <rybkolhoz@gmail.com>
2024-06-04 09:55:31 +0300

    create session method divided and minor fixes

--- src/main/java/com/ua/accommodation/controller/PaymentController.java
+++ src/main/java/com/ua/accommodation/controller/PaymentController.java
@@ -1,11 +1,15 @@
 package com.ua.accommodation.controller;
 
-import com.ua.accommodation.dto.PaymentRequestDto;
-import com.ua.accommodation.dto.PaymentResponseDto;
+import com.ua.accommodation.dto.payment.PaymentRequestDto;
+import com.ua.accommodation.dto.payment.PaymentResponseDto;
 import com.ua.accommodation.service.StripeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,36 +19,62 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Payment Management System", description = "Operations pertaining to "
+        + "payments in the system")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/checkout")
 public class PaymentController {
     private final StripeService stripeService;
 
+    @Operation(
+            summary = "Create a new payment session",
+            description = "Creates a new payment session."
+    )
     @PostMapping("/payments")
     @ResponseBody
-    public PaymentResponseDto createSession(@RequestBody PaymentRequestDto sessionDto) {
-        return stripeService.createPaymentSession(sessionDto);
+    public PaymentResponseDto createSession(
+            @Valid @RequestBody PaymentRequestDto requestDto,
+            Authentication authentication
+    ) {
+        return stripeService.createPaymentSession(requestDto, authentication);
     }
 
+    @Operation(
+            summary = "Get a payment session by string id.",
+            description = "Get a payment session by string id."
+    )
     @GetMapping("/payments/{id}")
     @ResponseBody
     public PaymentResponseDto retrieveSession(@PathVariable String id) {
         return stripeService.retrieveSession(id);
     }
 
+    @Operation(
+            summary = "Handle cansel payment operation.",
+            description = "When you press the \"Back\" button you "
+                    + "will be redirected to this endpoint."
+    )
     @GetMapping("/payments/cancel")
     @ResponseBody
     public PaymentResponseDto handleCancel(@RequestParam("session_id") String sessionId) {
         return stripeService.retrieveSession(sessionId);
     }
 
+    @Operation(
+            summary = "Handle success payment operation.",
+            description = "When you confirm the payment you will be redirected to this endpoint."
+    )
     @GetMapping("/payments/success")
     @ResponseBody
     public PaymentResponseDto handleSuccess(@RequestParam("session_id") String sessionId) {
         return stripeService.retrieveSession(sessionId);
     }
 
+    @Operation(
+            summary = "Get all payment operations by users id.",
+            description = "Get all payment operations by users id."
+    )
     @GetMapping("/payments")
     @ResponseBody
     public List<PaymentResponseDto> findPayments(
