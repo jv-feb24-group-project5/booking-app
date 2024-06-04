b05ac7451cac4d068b3b75fe117427a74a956192
Pavlo Betsa <bigworld2303@gmail.com>
2024-06-04 20:27:43 +0300

    Super hard fix

--- src/main/java/com/ua/accommodation/model/User.java
+++ src/main/java/com/ua/accommodation/model/User.java
@@ -40,7 +40,7 @@ public class User implements UserDetails {
     private String password;
     @Column(nullable = false)
     private boolean isDeleted;
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
