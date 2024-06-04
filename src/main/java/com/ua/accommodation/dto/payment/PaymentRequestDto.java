57ca463d70057aff98b04841eac38315e2162691
Олександр <91253664+MolchanovAlexander@users.noreply.github.com>
2024-06-04 14:11:01 +0300

    Task/Implement find payments by user id. (#23)
    Implement find payments by user id

--- src/main/java/com/ua/accommodation/dto/payment/PaymentRequestDto.java
+++ src/main/java/com/ua/accommodation/dto/payment/PaymentRequestDto.java
@@ -1,16 +1,13 @@
 package com.ua.accommodation.dto.payment;
 
+import jakarta.validation.constraints.Min;
 import java.math.BigDecimal;
-import java.util.Map;
 import lombok.Data;
 
 @Data
 public class PaymentRequestDto {
-
-    private String bookingId;
-    private String userEmail;
-    private String userName;
-    private String message;
+    @Min(1)
+    private Long bookingId;
+    @Min(1)
     private BigDecimal amount;
-    private Map<String, String> data;
 }
