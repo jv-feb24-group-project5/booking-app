28ac7480445c5ae7c3047146ee5a8478894723eb
Anton Haiduk <157686954+TonyH277@users.noreply.github.com>
2024-06-04 14:27:05 +0300

    Task/Add events to services (#19)
    * Add events to services
    
    * remove string concatenation at publishEvent methods
    
    * Task/scheduled event (#21)
    
    * switch string builder to string concatenation
    
    * resolve merging conflicts
    ---------
    
    Co-authored-by: TonyH277 <gajjduktoxa@gmail.com>
    Co-authored-by: Vladyslav Radko <124697095+VolandevlodD@users.noreply.github.com>

--- src/main/java/com/ua/accommodation/model/Address.java
+++ src/main/java/com/ua/accommodation/model/Address.java
@@ -8,11 +8,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "addresses")
 @Getter
 @Setter
+@ToString
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
